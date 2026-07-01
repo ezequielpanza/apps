5220c2e532c0c7181978d6b77cf7f9a205caa2cf
ezequielpanza <ezequielpanza@gmail.com>
2026-07-01 18:54:37 -0400

    Remove obsolete test city patch

--- apps/wander-travel/disable-city-test.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  if (window.__disableCityTest) return;
-  window.__disableCityTest = true;
-  const card = document.querySelector('.developer-city-card');
-  if (card) card.remove();
-})();
