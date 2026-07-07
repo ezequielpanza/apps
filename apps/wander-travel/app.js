810289b8d60634db520c9d2c6cbf2c3736f961c4
ezequielpanza <ezequielpanza@gmail.com>
2026-07-06 20:28:58 -0400

    update map cache

--- apps/wander-travel/app.js
+++ apps/wander-travel/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const MAP_RUNTIME_VERSION = '20260706-05';
+  const MAP_RUNTIME_VERSION = '20260707-02';
 
   document.write('<script src="runtime-map-core.js?v=' + MAP_RUNTIME_VERSION + '"><\/script>');
   document.write('<script src="runtime-map-position.js?v=' + MAP_RUNTIME_VERSION + '"><\/script>');
