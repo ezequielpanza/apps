e823dae2be11a9b7c646eb589b4db687e5c64ec2
ezequielpanza <ezequielpanza@gmail.com>
2026-07-08 15:52:09 -0400

    chore(wander): refresh map runtime for simulator long press

--- apps/wander-travel/app.js
+++ apps/wander-travel/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const MAP_RUNTIME_VERSION = '20260707-02';
+  const MAP_RUNTIME_VERSION = '20260708-01';
 
   document.write('<script src="runtime-map-core.js?v=' + MAP_RUNTIME_VERSION + '"><\/script>');
   document.write('<script src="runtime-map-position.js?v=' + MAP_RUNTIME_VERSION + '"><\/script>');
