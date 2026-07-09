2d382340650742e5c51a1507fb7370698d8adbb8
ezequielpanza <ezequielpanza@gmail.com>
2026-07-09 09:37:06 -0400

    chore(wander): publish build v0.81.1

--- apps/wander-travel/app.js
+++ apps/wander-travel/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const APP_BUILD = 'v0.81.0';
+  const APP_BUILD = 'v0.81.1';
   const MAP_RUNTIME_VERSION = '20260708-03';
 
   document.write('<script src="runtime-map-core.js?v=' + MAP_RUNTIME_VERSION + '"><\/script>');
