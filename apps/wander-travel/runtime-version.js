8b004792645e877ecc00b3f4372870dfe94ba86f
ezequielpanza <ezequielpanza@gmail.com>
2026-07-11 19:11:03 -0400

    Wander Travel v0.86.1 update displayed version

--- apps/wander-travel/runtime-version.js
+++ apps/wander-travel/runtime-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = 'v0.86.0';
+  const VERSION = 'v0.86.1';
   document.title = 'Wander Travel ' + VERSION;
   const drawerVersion = document.querySelector('#drawer-version');
   if (drawerVersion) drawerVersion.textContent = VERSION;
