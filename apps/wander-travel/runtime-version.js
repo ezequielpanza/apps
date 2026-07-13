9b355039317ad8207332e5322d08f38cc731ecc2
ezequielpanza <ezequielpanza@gmail.com>
2026-07-12 22:09:34 -0400

    Set Wander version v0.87.6

--- apps/wander-travel/runtime-version.js
+++ apps/wander-travel/runtime-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = 'v0.87.5';
+  const VERSION = 'v0.87.6';
   document.title = 'Wander Travel ' + VERSION;
   const drawerVersion = document.querySelector('#drawer-version');
   if (drawerVersion) drawerVersion.textContent = VERSION;
