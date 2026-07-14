f4292e9991bf915b4b8d59ed5a06a5ab241fb7ec
ezequielpanza <ezequielpanza@gmail.com>
2026-07-13 20:09:40 -0400

    Set runtime version to v0.88.0

--- apps/wander-travel/runtime-version.js
+++ apps/wander-travel/runtime-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = 'v0.87.9';
+  const VERSION = 'v0.88.0';
   document.title = 'Wander Travel ' + VERSION;
   const drawerVersion = document.querySelector('#drawer-version');
   if (drawerVersion) drawerVersion.textContent = VERSION;
