840aacfa3e3780b978fa293785b421a8ef1ae4d0
ezequielpanza <ezequielpanza@gmail.com>
2026-07-14 00:27:13 -0400

    Expose Wander v0.88.6

--- apps/wander-travel/runtime-version.js
+++ apps/wander-travel/runtime-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = 'v0.88.5';
+  const VERSION = 'v0.88.6';
   document.title = 'Wander Travel ' + VERSION;
   const drawerVersion = document.querySelector('#drawer-version');
   if (drawerVersion) drawerVersion.textContent = VERSION;
