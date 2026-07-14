a652e5bf048124b0ed54c7084d3fde7cc7d127d5
ezequielpanza <ezequielpanza@gmail.com>
2026-07-14 00:27:04 -0400

    Bump Wander to v0.88.6

--- apps/wander-travel/app.js
+++ apps/wander-travel/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const APP_BUILD = 'v0.88.5';
+  const APP_BUILD = 'v0.88.6';
   const MAP_RUNTIME_VERSION = '20260708-03';
 
   document.write('<script src="runtime-map-core.js?v=' + MAP_RUNTIME_VERSION + '"><\/script>');
