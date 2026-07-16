0a698ad5eedc70cc7bc2163028c3a24772d288ed
ezequielpanza <ezequielpanza@gmail.com>
2026-07-16 14:24:02 -0400

    Bump Wander build for personal POI context

--- apps/wander-travel/app.js
+++ apps/wander-travel/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const APP_BUILD = 'v0.92.47';
+  const APP_BUILD = 'v0.92.48';
   const MAP_RUNTIME_VERSION = '20260708-03';
 
   document.write('<script src="runtime-map-core.js?v=' + MAP_RUNTIME_VERSION + '"><\/script>');
