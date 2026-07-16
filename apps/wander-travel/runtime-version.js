ee1f6b897dae68e2d3e0c9383601b0b1173b412f
ezequielpanza <ezequielpanza@gmail.com>
2026-07-16 01:00:58 -0400

    Bump Wander Travel to v0.92.19

--- apps/wander-travel/runtime-version.js
+++ apps/wander-travel/runtime-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = 'v0.92.18';
+  const VERSION = 'v0.92.19';
   document.title = 'Wander Travel ' + VERSION;
   const drawerVersion = document.querySelector('#drawer-version');
   if (drawerVersion) drawerVersion.textContent = VERSION;
