a87cdfe8c40081e55a75ba072a13c406887b07be
ezequielpanza <ezequielpanza@gmail.com>
2026-07-16 15:08:07 -0400

    Update app build to v0.92.49

--- apps/wander-travel/app.js
+++ apps/wander-travel/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const APP_BUILD = 'v0.92.48';
+  const APP_BUILD = 'v0.92.49';
   const MAP_RUNTIME_VERSION = '20260708-03';
 
   document.write('<script src="runtime-map-core.js?v=' + MAP_RUNTIME_VERSION + '"><\/script>');
@@ -34,7 +34,7 @@
 
     try {
       loadStyle('wander-rule-checker.css?v=20260714-05');
-      await loadRuntime('runtime-situation-engine.js?v=20260714-08');
+      await loadRuntime('runtime-situation-engine.js?v=20260716-35');
       await loadRuntime('runtime-rule-checker.js?v=20260714-05');
     } catch {}
 
