7e0760cb610fdf84ce7d93b66a690170f95cf7c9
ezequielpanza <ezequielpanza@gmail.com>
2026-07-28 10:25:04 -0400

    Bump Wander web version to v0.109.3

--- apps/wander-travel/runtime-version.js
+++ apps/wander-travel/runtime-version.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = 'v0.109.2';
+  const VERSION = 'v0.109.3';
   const globalScope = typeof window !== 'undefined' ? window : self;
   globalScope.WanderVersion = VERSION;
   globalScope.WanderWebVersion = VERSION;
