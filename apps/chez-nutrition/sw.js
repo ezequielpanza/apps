2f7f1245a9b3e80731519af02a1263c5b018492b
ezequielpanza <ezequielpanza@gmail.com>
2026-08-17 14:23:14 -0400

    Refresh Chez Nutrition PWA cache

--- apps/chez-nutrition/sw.js
+++ apps/chez-nutrition/sw.js
@@ -1,4 +1,4 @@
-const CACHE='chez-nutrition-v1';
+const CACHE='chez-nutrition-v2';
 const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
