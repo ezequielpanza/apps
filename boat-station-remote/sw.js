22bb9236b0a8bb7847f7a0481cbea84b5415994d
ezequielpanza <ezequielpanza@gmail.com>
2026-08-21 08:11:55 -0400

    Refresh Boat Station Remote 0.2.0 cache

--- boat-station-remote/sw.js
+++ boat-station-remote/sw.js
@@ -1,4 +1,4 @@
-const CACHE='boat-station-remote-0.1.5-logo';
+const CACHE='boat-station-remote-0.2.0';
 const ASSETS=['./','index.html','manifest.webmanifest','icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
