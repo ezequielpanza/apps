e7588ac3bd2b2cf5f609f160713b397005d7e16c
ezequielpanza <ezequielpanza@gmail.com>
2026-08-21 18:03:32 -0400

    Cache stable Core compatibility layer in Boat Station PWA

--- apps/boat-station/web/sw.js
+++ apps/boat-station/web/sw.js
@@ -1,5 +1,5 @@
-const CACHE='boat-station-web-0.2.2';
-const ASSETS=['./','./index.html','./patch_v101.js','./patch_v200.js','./icon.png','./manifest.webmanifest','./VERSION'];
+const CACHE='boat-station-web-0.2.3';
+const ASSETS=['./','./index.html','./patch_v101.js','./patch_v200.js','./core_bridge_compat.js','./remote_gate.js','./remote_sync.js','./apk_link.js','./backend_url.js','./icon.png','./manifest.webmanifest','./VERSION'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('boat-station-web-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
